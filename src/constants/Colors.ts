c8db5ffa067fcaa6552fe7d804ae125c182faf6c
ivanviveros007 <iviveros.dev@gmail.com>
2024-09-20 19:05:53 -0300

    marshland palette

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,24 +3,37 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+  },
+  marshland: {
+    50: "#f7f7f6",
+    100: "#e7e5e0",
+    200: "#cecbc1",
+    300: "#adaa9b",
+    400: "#8c8a75",
+    500: "#716f5b",
+    600: "#595848",
+    700: "#49483c",
+    800: "#3d3c32",
+    900: "#34342d",
+    950: "#191914",
   },
 };
